1623d488189a61c547d015172af8528bf7af4015
Ho Tran Tien <trantien100700@gmail.com>
2024-12-18 19:31:14 +0700

    fix route

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
 import { Button } from '../ui/button'
 import { Link } from 'react-router-dom'
 import { UserButton, useUser } from '@clerk/clerk-react'
 
 function Header() {
-    const { user, isSignedIn } = useUser();
+    const { isSignedIn } = useUser();
     return (
         <div className='p-3 px-5 flex justify-between shadow-md'>
-             <Link to={'/dashboard'}>
-            <img src='/logo.svg' className='cursor-pointer' width={100} height={100} />
+            <Link to={'/dashboard'}>
+                <img src='/logo.svg' className='cursor-pointer' width={100} height={100} />
             </Link>
             {isSignedIn ?
                 <div className='flex gap-2 items-center'>
