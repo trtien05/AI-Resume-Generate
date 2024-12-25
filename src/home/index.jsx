9a8b5d7143376485d46912528c82c4f75a47b8f3
Ho Tran Tien <trantien100700@gmail.com>
2024-12-25 19:37:11 +0700

    add background

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -9,9 +9,9 @@ function Home() {
   return (
     <div>
       <Header />
-      <div>
-        <section className=" z-50">
-          <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+      <div className="bg-[url('/background.jpg')] bg-cover bg-center">
+        <section className="z-50 ">
+          <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 " >
             <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
               <span className="text-xs bg-primary rounded-full text-white px-4 py-1.5 mr-3">New</span> <span className="text-sm font-medium">devezfasthost.com All new Apps</span>
               <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
@@ -39,7 +39,7 @@ function Home() {
             </div>
           </div>
         </section>
-        <section className="py-8 bg-white z-50 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <section className="py-8 z-50 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
           <h2 className="font-bold text-3xl">How It Works?</h2>
           <h2 className="text-md text-gray-500">Give mock interview in just 3 simplar easy step</h2>
 
