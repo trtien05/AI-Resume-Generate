0886a53bf69efb3addd8a4153bf1a45e9736e6f0
Ho Tran Tien <trantien100700@gmail.com>
2024-12-25 16:25:55 +0700

    add redirect

--- src/auth/sign-in/index.jsx
+++ src/auth/sign-in/index.jsx
@@ -3,7 +3,12 @@ import { SignIn } from '@clerk/clerk-react'
 function SignInPage() {
   return (
     <div className='flex justify-center my-20 items-center'>
-      <SignIn />
+      <SignIn
+        path="/auth/sign-in"
+        routing="path"
+        fallbackRedirectUrl="/dashboard" // URL sẽ redirect sau khi đăng nhập
+        forceRedirectUrl="/dashboard" // URL sẽ redirect sau khi đăng ký
+      />
     </div>
   )
 }
